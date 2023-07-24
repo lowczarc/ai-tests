98b99aae3160fb20337ddbcdaade522f8423a5ee
Lancelot Owczarczak <lancelot@owczarczak.fr>
2023-07-24 19:02:26 +0200

    :zap: Generate file references

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import fs from "fs";
 import * as t from "io-ts";
 import { generate } from "polyfact";
+import { generateReferenceForEachFile } from "ai-docs";
+
+import { getJSONFolderRepresentation } from "./folder_to_json";
 
 (async () => {
-    const res = await generate("Count from 1 to 10");
-
-    console.log(res);
-})()
+    const folderRoot = process.argv[2];
+    if (!folderRoot) {
+        console.error("Usage: ai-tests <folder>");
+        process.exit(1);
+    }
+    const res = await generateReferenceForEachFile(await getJSONFolderRepresentation(folderRoot), (ref) => console.log(ref.originalPath));
+    fs.writeFileSync("ai-tests.json", JSON.stringify(res, null, 2));
+})();
